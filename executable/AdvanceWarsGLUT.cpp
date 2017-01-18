335389365357bd58375d8dc5fc08a097088f78b0
Damo <27757251@qq.com>
2017-01-19 01:46:04 +0800

    Add much callback for AdvanceWarsGLUT

--- executable/AdvanceWarsGLUT.cpp
+++ executable/AdvanceWarsGLUT.cpp
@@ -1,9 +1,48 @@
 #include<GL/glut.h>
 #include"Renderer_BattleField.h"
+#include"EngineGLES.h"
 #include"Texture.h"
 #include<stdio.h>
 
 Texture tex;
+
+void timerFunc(int timerID){
+	//glutTimerFunc(500,&timerFunc,0);
+}
+void idleFunc(){}
+
+void keyboardFunc(unsigned char key, int x,int y){}
+void keyboardUpFunc(unsigned char key, int x,int y){}
+void specialFunc(int key,int x,int y){}
+void specialUpFunc(int key,int x,int y){}
+void joystickFunc(unsigned int buttonMask,int x,int y,int z){}
+void reshapeFunc(int w,int h){}
+void visibilityFunc(int state){
+	switch(state){
+		case GLUT_VISIBLE:break;
+		case GLUT_NOT_VISIBLE:break;
+	}
+}
+void mouseFunc(int button,int state,int x,int y){
+	switch(button){
+		case GLUT_LEFT_BUTTON:break;
+		case GLUT_MIDDLE_BUTTON:break;
+		case GLUT_RIGHT_BUTTON:break;
+	}
+	switch(state){
+		case GLUT_UP:break;
+		case GLUT_DOWN:break;
+	}
+}
+void motionFunc(int x,int y){}
+void passiveMotionFunc(int x,int y){}
+void entryFunc(int state){
+	switch(state){
+		case GLUT_LEFT:break;
+		case GLUT_ENTERED:break;
+	}
+}
+
 void displayFunc(){
 	glClearColor(0,0,0,1);
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
@@ -42,17 +81,26 @@ int main(int argc,char* argv[]){
 	glutInitWindowSize(w,h);
 	auto window=glutCreateWindow("AdvanceWars");
 	//callback
+	glutTimerFunc(500,timerFunc,0);
+	glutIdleFunc(idleFunc);
+
+	glutKeyboardFunc(keyboardFunc);
+	glutKeyboardUpFunc(keyboardUpFunc);
+	glutSpecialFunc(specialFunc);
+	glutSpecialUpFunc(specialUpFunc);
+	glutJoystickFunc(joystickFunc,1);
+	glutReshapeFunc(reshapeFunc);
+	glutVisibilityFunc(visibilityFunc);
 	glutDisplayFunc(displayFunc);
-	//make 0,0 to left
-	//glScalef(2.0/w,2.0/h,1);
-	//glTranslatef(-w/2+1,-h/2+1,0);
-	//gl function
-	glEnable(GL_TEXTURE_2D);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+	glutMouseFunc(mouseFunc);
+	glutMotionFunc(motionFunc);
+	glutPassiveMotionFunc(passiveMotionFunc);
+	glutEntryFunc(entryFunc);
+	EngineGLES engine;
+	engine.initial(w,h);
 	//load texture
 	if(argc==2){
-		FilePNG fileBmp;
+		FileBMP fileBmp;
 		fileBmp.loadFile(argv[1]);
 		fileBmp.parseData();
 		tex.texImage2D(fileBmp);
