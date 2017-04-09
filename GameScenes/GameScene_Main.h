f11f4393b7c69df4b6586f6f6c0b01c089aadce1
Damo <27757251@qq.com>
2017-04-10 02:16:20 +0800

    Add something....

---
+++ GameScenes/GameScene_Main.h
@@ -0,0 +1,19 @@
+#ifndef GAMESCENE_MAIN_H
+#define GAMESCENE_MAIN_H
+
+#include"GameScene.h"
+#include"List.h"
+#include<string>
+
+//主场景,显示一下商标
+class GameScene_Main:public GameScene{
+	GameScene_Main();
+	void consumeTimeSlice();
+private:
+	int status;
+	int countDown;//倒计时
+	int contentBrightness;//内容亮度
+	//
+	List<string> menuItemNames;
+};
+#endif
